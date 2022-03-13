d0ee28743a1077e076dffd415e53ab13fabfb5cf
Laveena1208 <2019laveena.kithani@ves.ac.in>
2022-03-13 20:55:05 +0530

    add part done

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,31 @@
 $(function() {
     $('.modal').modal();
+
+    // function getQueryString()
+    // {
+    //     //let url = windows.location.href;
+    //     //let indexOfQuesMark = url.indexOf('?');
+    //     // let queryString = yrl.slice(indexOfQuesMark+1);
+    //     //let queryParams = queryString.split('&');
+    //     let queryParams = window.location.href.slice(window.location.href.indexOf('?')+1).split('&');
+    //     let vars = [];
+    //     queryParams.forEach(param =>{
+    //         let pair = param.split('=');
+    //         let key = pair[0];
+    //         let value =  pair[1];
+    //         vars[key] = value;
+    //     });
+    //     return vars;
+
+    // }
+    // let query = getQueryString();
+    // let operation = query['op'];
+    // let status = query['status'];
+    // if(operation === 'add' && status === 'success')
+    // {
+    //     Map.toast({
+    //         html: 'Contact added sucessfully',
+    //         classess: 'green darken-1'
+    //     });
+    // }
 });
